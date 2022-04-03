4ee564969d2f6fb3b02ac5d84a675bfbc43b26dc
Nonso Andrew <nonsoandrew1@gmail.com>
2022-04-03 16:48:30 +0100

    My changes

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,35 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amount);
   YourToken public yourToken;
+  uint256 public constant tokensPerEth = 100;
+
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() payable public {
+    uint amountOfTokens = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+   
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
+  function withdraw() public payable onlyOwner{
+     
+        yourToken.transfer(msg.sender, msg.value);
+  }
   // ToDo: create a sellTokens() function:
+  function sellTokens(uint256 amount) public payable{
+    
+    yourToken.transferFrom(msg.sender, address(this), amount);
+    payable(msg.sender).transfer(amount/tokensPerEth);
+    emit SellTokens(address(this), amount);
+  }
 
 }
